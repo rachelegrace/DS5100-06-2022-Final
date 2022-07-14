40c6ea4323feb0cafb26a544887f3ff610204bbb
rachelegrace <94571546+rachelegrace@users.noreply.github.com>
2022-07-14 09:40:11 -0400

    Update directory

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DS5100-2022-06-Final',
+    version='1.0.0',
+    url='https://github.com/rachelegrace/DS5100-2022-06-Final.git',
+    author='Rachel Grace',
+    author_email='rg5xm@virginia.edu',
+    description='DS5100 Final Project - Montecarlo Simulator',
+    packages=find_packages(),    
+    install_requires=['numpy', 'pandas', 'random', 'IPython.display'],
+)
